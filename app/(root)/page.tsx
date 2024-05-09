d2979ef528ddc191b6f4d9a8065edaffb3a82937
Milton Palmeira <lpsantana_@hotmail.com>
2024-05-09 11:21:48 -0300

    add(form): providers and modal form added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,23 @@
-import { Button } from '@/components/ui/button';
+"use client";
+
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 export default function Home() {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
   return (
-    <div className='p-4'>
-      <Button size='lg'>Clique aqui</Button>
+    <div className="p-4">
+      
     </div>
   );
 }
+
